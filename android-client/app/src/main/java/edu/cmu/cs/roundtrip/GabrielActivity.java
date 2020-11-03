9d261bc1000052e9881a8e90b835d38c5ee97c27
Roger Iyengar <ri@rogeriyengar.com>
2020-11-03 01:28:17 -0500

    New Gabriel Version

--- android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
+++ android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
@@ -1,92 +1,53 @@
 package edu.cmu.cs.roundtrip;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.util.Size;
-import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Locale;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
+import edu.cmu.cs.gabriel.camera.CameraCapture;
+import edu.cmu.cs.gabriel.camera.YuvToJpegConverter;
+import edu.cmu.cs.gabriel.camera.ImageViewUpdater;
+import edu.cmu.cs.gabriel.client.comm.ServerComm;
 import edu.cmu.cs.gabriel.client.results.ErrorType;
 import edu.cmu.cs.gabriel.protocol.Protos;
-import edu.cmu.cs.gabriel.protocol.Protos.ResultWrapper;
 import edu.cmu.cs.gabriel.protocol.Protos.InputFrame;
-import edu.cmu.cs.gabriel.client.comm.ServerComm;
+import edu.cmu.cs.gabriel.protocol.Protos.ResultWrapper;
 
 public class GabrielActivity extends AppCompatActivity {
     private static final String TAG = "GabrielActivity";
     private static final String SOURCE = "roundtrip";
-    private static final String[] REQUIRED_PERMISSIONS = new String[] {
-            Manifest.permission.CAMERA,
-    };
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
     private static final int WIDTH = 640;
     private static final int HEIGHT = 480;
 
     private ServerComm serverComm;
-    private TextToSpeech textToSpeech;
-    private ExecutorService cameraExecutor;
-    private PreviewView viewFinder;
-    private ImageView imageView;
-    private YuvToRgbConverter yuvToRgbConverter;
-    private Bitmap bitmap;
+    private YuvToJpegConverter yuvToJpegConverter;
+    private CameraCapture cameraCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         setContentView(R.layout.activity_gabriel);
 
-        if (allPermissionsGranted()) {
-            startCamera();
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-        }
-
-        TextToSpeech.OnInitListener onInitListener = i -> textToSpeech.setLanguage(Locale.US);
-        textToSpeech = new TextToSpeech(this, onInitListener);
-
-        cameraExecutor = Executors.newSingleThreadExecutor();
-        viewFinder = findViewById(R.id.viewFinder);
-        imageView = findViewById(R.id.imageView);
-        yuvToRgbConverter = new YuvToRgbConverter(this);
+        PreviewView viewFinder = findViewById(R.id.viewFinder);
+        ImageView imageView = findViewById(R.id.imageView);
+        ImageViewUpdater imageViewUpdater = new ImageViewUpdater(imageView);
 
         Consumer<ResultWrapper> consumer = resultWrapper -> {
             ResultWrapper.Result result = resultWrapper.getResults(0);
-            ByteString byteString = result.getPayload();
-            Bitmap bitmap = BitmapFactory.decodeByteArray(
-                    byteString.toByteArray(), 0, byteString.size());
-            viewFinder.post(() -> imageView.setImageBitmap(bitmap));
+            ByteString jpegByteString = result.getPayload();
+
+            imageViewUpdater.accept(jpegByteString);
         };
 
         Consumer<ErrorType> onDisconnect = errorType -> {
@@ -96,34 +57,20 @@ public class GabrielActivity extends AppCompatActivity {
 
         serverComm = ServerComm.createServerComm(
                 consumer, BuildConfig.GABRIEL_HOST, 9099, getApplication(), onDisconnect);
+
+        yuvToJpegConverter = new YuvToJpegConverter(this);
+        cameraCapture = new CameraCapture(this, analyzer, WIDTH, HEIGHT, viewFinder);
     }
 
-    @SuppressLint("UnsafeExperimentalUsageError")
     final private ImageAnalysis.Analyzer analyzer = new ImageAnalysis.Analyzer() {
         @Override
         public void analyze(@NonNull ImageProxy image) {
             serverComm.sendSupplier(() -> {
-                if (bitmap == null) {
-                    bitmap = Bitmap.createBitmap(
-                            image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
-                }
-                yuvToRgbConverter.yuvToRgb(image.getImage(), bitmap);
-
-                // Images are passed to this method without being rotated
-                Matrix matrix = new Matrix();
-                matrix.postRotate(image.getImageInfo().getRotationDegrees());
-
-                // rotatedBitmap must be its own variable because the dimensions are different
-                // from those of bitmap
-                Bitmap rotatedBitmap = Bitmap.createBitmap(
-                        bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                ByteString jpegByteString = yuvToJpegConverter.convertToJpeg(image);
 
                 return InputFrame.newBuilder()
                         .setPayloadType(Protos.PayloadType.IMAGE)
-                        .addPayloads(ByteString.copyFrom(byteArrayOutputStream.toByteArray()))
+                        .addPayloads(jpegByteString)
                         .build();
             }, SOURCE, false);
 
@@ -131,63 +78,9 @@ public class GabrielActivity extends AppCompatActivity {
         }
     };
 
-    private void startCamera() {
-        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
-                ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                Preview preview = new Preview.Builder().build();
-                preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
-
-                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
-
-                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
-                        .setTargetResolution(new Size(WIDTH, HEIGHT))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build();
-                imageAnalysis.setAnalyzer(cameraExecutor, analyzer);
-
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll();
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                        GabrielActivity.this, cameraSelector, preview, imageAnalysis);
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String requiredPermission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, requiredPermission) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        cameraExecutor.shutdown();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera();
-            } else {
-                Toast.makeText(this,
-                        "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
+        cameraCapture.shutdown();
     }
 }
